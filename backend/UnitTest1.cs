7563773d11e0d86bf81e6d32aa00e98f6c7a41f1
student-wroclaw <student-wroclaw@EDU-WSB.local>
2026-04-19 14:31:02 +0200

    Dodanie testu jednostkowego

---
+++ backend/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Xunit;
+using Backend.Models;
+
+namespace Backend.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void NewTask_ShouldNotBeCompleted()
+        {
+            var task = new CloudTask { Name = "Przetestować bezpiecznik" };
+
+
+            Assert.False(task.IsCompleted);
+        }
+    }
+}
